6d1ff56da579a418be274ba67e19fd0f39052581
赵吉彤 <zjtong3576@sina.com>
2016-11-19 22:56:28 +0800

    scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var $ = require('gulp-load-plugins')()
 
 gulp.task('styles', function () {
     return gulp
-        .src('*.scss')
+        .src('scss/*.scss')
         .pipe($.sass({ outputStyle: 'compressed' }))
         .pipe($.autoprefixer('last 2 version'))
-        .pipe(gulp.dest(''))
+        .pipe(gulp.dest('./src/'))
 })
 
 gulp.task('watch', ['default'], function () {
-    gulp.watch('*.scss', ['styles'])
+    gulp.watch('scss/*.scss', ['styles'])
 })
 
 gulp.task('default', ['styles'])
